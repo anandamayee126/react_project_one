0f9d53083a896ebd67b46f055a45d3a852647b6e
Anandamayee Ghosh <65214830+anandamayee126@users.noreply.github.com>
2024-04-02 21:33:08 +0530

    wrote complex JSX code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 export default function ExpenseItem(){
-    return(
-        <ul>
-            <li>Food - $5</li>
-            <li>Petrol - $20</li>
-            <li>Movie - $10</li>
-        </ul>
+    return (
+      <div>
+        <div>15 August 2023</div>
+        
+        <div><h2>Location</h2>
+          <p>Delhi</p>
+        </div>
+  
+        <div><h2>Book</h2>
+          <p>$10</p>
+        </div>
+        <div></div>
+      </div>
     );
-}
+  }
+  
