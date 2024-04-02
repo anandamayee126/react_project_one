b4e88a713c0126641b4ce804cc1c645bd2cb2959
Anandamayee Ghosh <65214830+anandamayee126@users.noreply.github.com>
2024-04-02 23:14:37 +0530

    Added CSS

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
-export default function ExpenseItem(){
-    return (
-      <div>
-        <div>15 August 2023</div>
-        
-        <div><h2>Location</h2>
-          <p>Delhi</p>
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  return (
+    <div>
+      <div className="expense-item">15 August 2023</div>
+       <div className="expense-item__location"><h2 className="expense-item h2">Location</h2>
+          <div className="expense-item__location">Delhi</div>
         </div>
-  
-        <div><h2>Book</h2>
-          <p>$10</p>
-        </div>
-        <div></div>
+      <div className="expense-item__description">
+        <h2 className="expense-item h2">Book</h2>
+        <div className="expense-item__price">$10</div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
+
+export default ExpenseItem;
