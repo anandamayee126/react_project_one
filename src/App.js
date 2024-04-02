843d9efde34ed925079e8e5c9b3e5ace05101a2b
Anandamayee Ghosh <65214830+anandamayee126@users.noreply.github.com>
2024-04-02 20:42:17 +0530

    first component inside the component folder added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
       <h1> Let's get started</h1>
-      <p> React app created</p>
-      
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
