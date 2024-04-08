dc7dca9642e4dc77213638637b100c9447d11836
Anandamayee Ghosh <65214830+anandamayee126@users.noreply.github.com>
2024-04-08 19:20:22 +0530

    props added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,14 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  let expenseLocation="Bangalore";
+function ExpenseItem(props) {
+
   return (
     <div>
-      <div className="expense-item">15 August 2023</div>
-       <div className="expense-item__location"><h2 className="expense-item h2">Location</h2>
-          <div className="expense-item__location">{expenseLocation}</div>
-        </div>
+      <div className="expense-item">{props.date.toISOString()}</div>
+       <div className="expense-item__location"><h2 className="expense-item h2">{props.location}</h2></div>
       <div className="expense-item__description">
-        <h2 className="expense-item h2">Book</h2>
-        <div className="expense-item__price">$10</div>
+        <h2 className="expense-item h2">{props.title}</h2>
+        <div className="expense-item__price">{props.price}</div>
       </div>
     </div>
   );
