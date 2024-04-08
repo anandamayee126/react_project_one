4df72e97b89def9b039df6c1dd4c9b82c2226f2e
Anandamayee Ghosh <65214830+anandamayee126@users.noreply.github.com>
2024-04-08 19:01:52 +0530

    Added variables

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  let expenseLocation="Bangalore";
   return (
     <div>
       <div className="expense-item">15 August 2023</div>
        <div className="expense-item__location"><h2 className="expense-item h2">Location</h2>
-          <div className="expense-item__location">Delhi</div>
+          <div className="expense-item__location">{expenseLocation}</div>
         </div>
       <div className="expense-item__description">
         <h2 className="expense-item h2">Book</h2>
