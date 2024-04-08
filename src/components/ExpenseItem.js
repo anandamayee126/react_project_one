937dad384797f16d4500bf8c357b12af1d0a053f
Anandamayee Ghosh <65214830+anandamayee126@users.noreply.github.com>
2024-04-08 23:09:56 +0530

    calender type date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-
+let month=props.date.getMonth();
+let year= props.date.getFullYear();
+let date= props.date.getDate();
   return (
     <div>
-      <div className="expense-item">{props.date.toISOString()}</div>
+      <div className="expense-item">
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{date}</div>
+      </div>
        <div className="expense-item__location"><h2 className="expense-item h2">{props.location}</h2></div>
       <div className="expense-item__description">
         <h2 className="expense-item h2">{props.title}</h2>
