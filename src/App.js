6fc11836b954d59c22e5290dc0a89d0556d89f75
Anandamayee Ghosh <65214830+anandamayee126@users.noreply.github.com>
2024-04-08 22:14:13 +0530

    props added along with looping using map

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  let expenses=[
+    {id:1,date:new Date(2023,7,12),location:"Bangalore",title:"pen",price:"$1"},
+    {id:2,date:new Date(2023,8,12),location:"Bangalore",title:"book",price:"$10"},
+    {id:3,date:new Date(2023,9,12),location:"Bangalore",title:"jwellery",price:"$15"},
+    {id:4,date:new Date(2023,10,12),locatio:"Bangalore",title:"plant",price:"$20"}
+  ]
   return (
       <div>
         <h1>Let's get Started</h1>
-        <ExpenseItem
-          date={new Date(2023, 7, 15)}
-           location="Bangalore"
-          title="Insurance"
-          price="50"
-        ></ExpenseItem>
-        <ExpenseItem
-          date={new Date(2023, 3, 25)}
-         location="Delhi"
-          title="Book"
-          price="20"
-        ></ExpenseItem>
-        <ExpenseItem
-          date={new Date(2023, 10, 11)}
-          location="Hyderabad"
-          title="Pen"
-          price="5"
-        ></ExpenseItem>
-        <ExpenseItem
-          date={new Date(2023, 1, 14)}
-          location="Mumbai"
-          title="Laptop"
-          price="200"
-        ></ExpenseItem>
+        {
+          expenses.map((exp,ind)=>{
+            <ExpenseItem
+            key={exp.id}
+            date={exp.date}
+            location={exp.location}
+            title={exp.title}
+            price={exp.price}
+          ></ExpenseItem>
+          })
+          
+        }
+        
       </div>
     );
 }
